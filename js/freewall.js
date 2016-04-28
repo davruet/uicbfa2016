1532b7da759e22cba2c97cdd9c63391f53536291
paulzdon <Paul@dhcp-18-203-251.nat.wireless.uic.edu>
2016-04-28 09:02:34 -0500

    freewall top margin fix

--- js/freewall.js
+++ js/freewall.js
@@ -27,7 +27,7 @@
             //fixSize: 0, resize but keep ratio = no fill gap;
             //fixSize: 1, no resize + no adjust = no fill gap;
             gutterX: 50, // width spacing between blocks;
-            gutterY: 500, // height spacing between blocks;
+            gutterY: 250, // height spacing between blocks;
             keepOrder: false,
             selector: '> div',
             draggable: false,
